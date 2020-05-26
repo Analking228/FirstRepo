f5591b69d1de8dacf69307cf4e1f24038c7fa580
AnaKing228 <kronos5525@yandex.ru>
2020-05-27 00:20:02 +0300

    fck split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cjani <cjani@studen.21-school.ru>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/18 11:38:44 by flexer            #+#    #+#             */
-/*   Updated: 2020/05/26 23:46:25 by cjani            ###   ########.fr       */
+/*   Updated: 2020/05/27 00:18:44 by cjani            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ static char		**ft_split_split_alloc(char **sp, char *s, char c)
 		if (s[i] != '\0')
 			if ((s[i] == c && count != 0) || (s[i] != c && s[i + 1] == '\0'))
 			{
-				sp[j] = (char *)ft_calloc(count + 2, sizeof(char));
+				sp[j] = (char *)ft_calloc(count + 1, sizeof(char));
 				if (!sp[j])
 					return (NULL);
 				count = 0;
@@ -93,12 +93,10 @@ char			**ft_split_injection(char **sp, char *s, char c)
 
 void			ft_free_mem(char **split, char *s, char c)
 {
-	size_t		j;
 	size_t		i;
 	size_t		splitc;
 
 	i = 0;
-	j = 0;
 	splitc = 0;
 	if (s[i] != c)
 		splitc++;
@@ -109,10 +107,9 @@ void			ft_free_mem(char **split, char *s, char c)
 				splitc++;
 		i++;
 	}
-	while (j < splitc)
+	while (splitc--)
 	{
-		free(split[j]);
-		j++;
+		free(split[splitc]);
 	}
 }
 
